50e1055315220dbb750ef8d37e2259814593b349
Harun Bekcan <harunbekcan@gmail.com>
2023-03-24 16:53:30 +0300

    firstCommit

---
+++ EasyDatePicker/src/main/java/com/harunbekcan/easydatepicker/EasyDatePicker.kt
@@ -0,0 +1,11 @@
+package com.harunbekcan.easydatepicker
+
+import android.content.Context
+import android.widget.Toast
+
+class EasyDatePicker {
+
+    fun sampleToastMessage(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
